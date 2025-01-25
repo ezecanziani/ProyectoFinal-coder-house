1d78c7e39c2e36ce7d7440502701439bf4e958a8
ezecanziani <ezecanziani@gmail.com>
2025-01-25 20:45:14 -0300

    Update ProyectoFinalApplication.java

--- src/main/java/com/coderhouse/ProyectoFinalApplication.java
+++ src/main/java/com/coderhouse/ProyectoFinalApplication.java
@@ -2,8 +2,12 @@ package com.coderhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.coderhouse.repositories")
+@EntityScan(basePackages = "com.coderhouse.models")
 public class ProyectoFinalApplication {
 
 	public static void main(String[] args) {
